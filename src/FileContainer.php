7149e8a617e7e4a74d9ac72d016942ea6c48fae5
Kristen Gilden <kristen.gilden@gmail.com>
2014-04-13 22:03:00 +0300

    Implement file local listing & extracting
    Not 100% sure this is the way to go. A 3rd-party library could be used
    instead to read a local archive. This implementation lacks of retrieving
    the file list from the digidoc service.

--- src/FileContainer.php
+++ src/FileContainer.php
@@ -85,6 +85,55 @@ class FileContainer implements \Serializable
     }
 
     /**
+     * Gets a list of files in the current container. Metadata files are
+     * ignored. The Api is used, if this container is not saved yet.
+     *
+     * @todo tests
+     *
+     * @return string[]
+     */
+    public function listFiles()
+    {
+        if (false !== ($archive = $this->openArchive($this->container))) {
+            return $this->listArchiveFiles($archive);
+        }
+
+        throw new \Exception('Not implemented.');
+    }
+
+    /**
+     * Extracts the archive contents. Metadata is omitted.
+     *
+     * @todo Perhaps this should not be supported - the user can use ZipArchive
+     *       himself to do any extracting. However, FileContainer::listFiles
+     *       seems to be more necessary and it uses ZipArchive anyways so might
+     *       as well keep it.
+     *
+     * @todo tests
+     *
+     * @see http://www.php.net/manual/en/ziparchive.extractto.php
+     *
+     * @param string $destination Location where to extract the files
+     * @param mixed  $entries     The entries to extract (accepts single name or an array of names)
+     */
+    public function extractTo($destination, $entries = array())
+    {
+        if (false === ($archive = $this->openArchive($this->container))) {
+            throw new RuntimeException(sprintf('Cannot extract "%s", because it is not yet written on disk.', $this->container->getPathname()));
+        }
+
+        if (is_string($entries)) {
+            $entries = array();
+        }
+
+        foreach ($this->listArchiveFiles($archive) as $entry) {
+            if (!$entries || in_array($entry, $entries)) {
+                $archive->extractTo($destination, $entry);
+            }
+        }
+    }
+
+    /**
      * @return array
      */
     public function serialize()
@@ -142,5 +191,86 @@ class FileContainer implements \Serializable
         }
 
         return $this->api;
+    }
+
+    /**
+     * Lists the files in the given archive. We can tread BDOC files as Zip
+     * archives. @todo provide a link as a reference
+     *
+     * @param \ZipArchive $file
+     *
+     * @return array
+     */
+    private function listArchiveFiles(\ZipArchive $archive) {
+        $excludedPatterns = array(
+            '^mimetype$',
+            '^META-INF/'
+        );
+
+        $fileNames = array();
+
+        for ($i = 0; $i < $archive->numFiles; $i++) {
+            $fileName = $archive->getNameIndex($i);
+
+            if (!$this->patternsMatch($excludedPatterns, $fileName)) {
+                $fileNames[] = $fileName;
+            }
+        }
+
+        return $fileNames;
+    }
+
+    /**
+     * @param array  $patterns
+     * @param string $value
+     *
+     * @return boolean Whether any of the given patterns match the value
+     */
+    private function patternsMatch(array $patterns, $value)
+    {
+        foreach ($patterns as $pattern) {
+            if (preg_match('#'.$pattern.'#', $value)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param File $file
+     *
+     * @return \ZipArchive|boolean
+     */
+    private function openArchive(File $file)
+    {
+        if (!file_exists($file)) {
+            return false;
+        }
+
+        $archive = new \ZipArchive();
+
+        if (true !== ($result = $archive->open($file->getPathname()))) {
+            $codes = array(
+               10 => 'ZipArchive::ER_EXISTS',
+               21 => 'ZipArchive::ER_INCONS',
+               18 => 'ZipArchive::ER_INVAL',
+               14 => 'ZipArchive::ER_MEMORY',
+               9  => 'ZipArchive::ER_NOENT',
+               19 => 'ZipArchive::ER_NOZIP',
+               11 => 'ZipArchive::ER_OPEN',
+               5  => 'ZipArchive::ER_READ',
+               4  => 'ZipArchive::ER_SEEK',
+            );
+
+            throw new RuntimeException(sprintf(
+                'Failed to open "%s" archive, error %s. See %s for details',
+                $file->getPathname(),
+                isset($codes[$result]) ? $codes[$result] : $result),
+                'http://www.php.net/manual/en/ziparchive.open.php'
+            );
+        }
+
+        return $archive;
     }
 }
